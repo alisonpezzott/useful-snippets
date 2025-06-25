f632a233a7cb638b679e3890524bb478cdaefb94
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2025-06-25 13:43:15 -0300

    Update and rename alias_git_quick_push_bash.ps1 to alias_git_add_commit_push.bash

---
+++ git/alias_git_add_commit_push.bash
@@ -0,0 +1,2 @@
+# Run on Git Bash
+git config --global alias.acp '!git add . && git commit -m "$(date +%Y-%m-%d\ %H:%M:%S)" && git push'
--- git/alias_git_quick_push_bash.ps1
+++
@@ -1,2 +0,0 @@
-# Run on Git Bash
-git config --global alias.gqp '!git add . && git commit -m "$(date +%Y-%m-%d\ %H:%M:%S)" && git push'
