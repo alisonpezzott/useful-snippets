949fb5dec3559310472859d7e6df79d963fa96fc
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2025-12-15 22:38:52 -0300

    Refactor drop database script for SQL Server
    Updated the script to drop a specified database after checking and killing active sessions.

--- sql_databases/drop_db_sql_server.sql
+++ sql_databases/drop_db_sql_server.sql
@@ -1,13 +1,28 @@
+USE master;
+GO
+
+DECLARE @database NVARCHAR(128) = 'Sandbox';
+DECLARE @sql NVARCHAR(MAX);
+
 -- Check active sessions
-SELECT *
+SELECT session_id, login_name, host_name, program_name, status
 FROM sys.dm_exec_sessions
-WHERE database_id = DB_ID('NomeBanco')
+WHERE database_id = DB_ID(@database);
 
 -- Kill active sessions
-KILL 68 -- Substitute pelo ID da sessão que deseja
+SET @sql = '';
+SELECT @sql = @sql + 'KILL ' + CAST(session_id AS VARCHAR(10)) + '; '
+FROM sys.dm_exec_sessions
+WHERE database_id = DB_ID(@database)
+  AND session_id <> @@SPID; -- Don't kill your own session
+
+-- Execute kill commands
+IF @sql <> ''
+BEGIN
+    EXEC sp_executesql @sql;
+END
 
 -- Drop database
-USE [master]
-GO
-DROP DATABASE [NomeBanco]
+SET @sql = 'DROP DATABASE ' + QUOTENAME(@database);
+EXEC sp_executesql @sql;
 GO
