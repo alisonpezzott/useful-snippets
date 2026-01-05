b913fa320f60c25a62bbdaa0efb5768254570127
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2026-01-05 11:59:28 -0300

    Improve comments for import statements in import_helpers.py
    Updated comments to accurately describe import statements.

---
+++ fabric_notebooks/import_helpers.py
@@ -0,0 +1,7 @@
+# Import helpers from .py files
+
+import sys
+sys.path.append(f"/lakehouse/default/Files/libs/v0.0.1")
+
+from helpers import log_run # where helpers is a file and log_run a function  
+
