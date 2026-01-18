d52ead24295ad43d94fa9f7f543bb646b8d30cf8
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2026-01-17 22:02:51 -0300

    Add SQL command to export customer data to CSV

--- sql_databases/export_data_from_sql_server.ps1
+++ sql_databases/export_data_from_sql_server.ps1
@@ -17,3 +17,19 @@ Invoke-Sqlcmd -ServerInstance "YourServer" `
     -TrustServerCertificate | Export-Csv -Path "C:\temp\your_table.csv" `
     -Delimiter ";" `
     -NoTypeInformation
+
+
+Invoke-Sqlcmd -ServerInstance "YourServer" `
+    -Database "YourDatabase" `
+    -Username "YourUser" `
+    -Password "YourPassword" `
+    -Query ("SELECT customer_id, first_name, middle_name, last_name, address_line_1, " +
+			"addess_line_2, city, state_province_code, country_region_code, " +
+			"state_name, territory_name, territory_group, " +
+			"CONVERT(varchar(10), start_date, 23) AS start_date, " + 
+			"CONVERT(varchar(10), end_date, 23)   AS end_date,  " +  
+			"is_active " +
+			"FROM raw.customer ") `
+    -TrustServerCertificate | Export-Csv -Path "C:\temp\customer.csv" `
+    -Delimiter ";" `
+    -NoTypeInformation
