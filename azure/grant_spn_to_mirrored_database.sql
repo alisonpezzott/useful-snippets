823ace265b0afb17a3dc6b6a4f9142d5634252ab
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2026-03-21 20:50:12 -0300

    Add user and permissions for email

--- azure/grant_spn_to_mirrored_database.sql
+++ azure/grant_spn_to_mirrored_database.sql
@@ -8,3 +8,25 @@ GRANT ALTER ANY EXTERNAL MIRROR TO [AppFabric];
 GRANT VIEW DATABASE PERFORMANCE STATE TO [AppFabric];
 GRANT VIEW DATABASE SECURITY STATE TO [AppFabric];
 GO
+
+
+-- Other util queries
+SELECT name, type_desc
+FROM sys.database_principals
+WHERE name = 'alison.pezzott@overdax.com';
+
+CREATE USER [alison.pezzott@overdax.com] FROM EXTERNAL PROVIDER;
+GO
+
+ALTER ROLE db_datareader ADD MEMBER [alison.pezzott@overdax.com];
+GO
+
+ALTER ROLE db_datawriter ADD MEMBER [alison.pezzott@overdax.com];
+GO
+
+ALTER ROLE db_owner ADD MEMBER [alison.pezzott@overdax.com];
+GO
+
+GRANT ALTER ANY EXTERNAL MIRROR TO [AppFabric];
+GRANT VIEW DATABASE PERFORMANCE STATE TO [AppFabric];
+GRANT VIEW DATABASE SECURITY STATE TO [AppFabric];
