4973622b3caca3f4084313251a1515906d9155c5
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2026-03-28 08:08:58 -0300

    Exclude all branches except main and develop

--- git/exclude_all_branchs_except_main.ps1
+++ git/exclude_all_branchs_except_main.ps1
@@ -1,7 +1,7 @@
 git checkout main
 
 # local
-git branch | Where-Object { $_ -notmatch 'main' } | ForEach-Object { git branch -D $_.Trim() }
+git branch | Where-Object { $_ -notmatch '^\*' -and $_ -notmatch '\b(main|develop)\b' } | ForEach-Object { git branch -D $_.Trim() }
 
 # remote
-git branch -r | Where-Object { $_ -notmatch 'origin/main' } | ForEach-Object { git push origin --delete $_.Trim().Replace('origin/', '') }
+git branch -r | Where-Object { $_ -notmatch '^\*' -and $_ -notmatch '\b(origin/main|origin/develop)\b' } | ForEach-Object { git push origin --delete $_.Trim().Replace('origin/', '') }
